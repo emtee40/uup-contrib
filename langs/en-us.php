f587655e99a39180c64b14e53df56c49dc888957
orin <orin@git.uupdump.net>
2024-02-28 18:38:49 +0100

    Update translations

--- langs/en-us.php
+++ langs/en-us.php
@@ -19,6 +19,7 @@ $s['uupdumpSub'] = '%s - UUP dump'; //Browse known builds - UUP dump
 $s['build'] = 'Build';
 $s['arch'] = 'Architecture';
 $s['ring'] = 'Channel';
+$s['branch'] = 'Branch';
 $s['updateid'] = 'Update ID';
 $s['update'] = 'Update';
 $s['lang'] = 'Language';
@@ -82,6 +83,8 @@ $s['addNewBuild'] = 'Add a new build';
 $s['newBuildNextText'] = 'Click the <i>Next</i> button to start searching with the specified options.';
 $s['optionsNotice'] = 'Options notice';
 $s['optionsNoticeText'] = 'Options found here configure how the underlying Windows Update client reports itself to the Microsoft servers. It is crucial to set these properly, otherwise you will receive an error.';
+$s['autoSelect'] = 'Automatic selection';
+$s['thisOnly'] = 'Return the specified build only';
 
 //known.php
 $s['browseKnown'] = 'Browse known builds';
